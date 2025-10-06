5b027133a238159de1aaafe73b26e776fd8b1082
DinhKhanhMinh <minhdk.bi12-265@st.usth.edu.vn>
2025-10-06 22:10:49 +0700

    fixed file type from ts to mjs

---
+++ client/next.config.mjs
@@ -0,0 +1,16 @@
+
+/** @type {import('next').NextConfig;} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dkm-inventory-management-bucket.s3.ap-southeast-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
